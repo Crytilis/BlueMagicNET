a452d1a579cfeacb9dac8bd13aff9afab668fc4b
Ashley <saeshyls@gmail.com>
2016-06-23 12:24:04 +0100

    Fix Typos and Improve Error Messages in Native/Methods.cs

--- BlueMagic/Native/Methods.cs
+++ BlueMagic/Native/Methods.cs
@@ -20,7 +20,7 @@ namespace BlueMagic.Native
         {
             int pId = Imports.GetProcessId(processHandle);
             if (pId == 0)
-                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to get Id from process handle {1}",
+                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to get Id from process handle 0x{1}",
                     Marshal.GetLastWin32Error(), processHandle.DangerousGetHandle().ToString("X")));
             return pId;
         }
@@ -29,7 +29,7 @@ namespace BlueMagic.Native
         {
             bool Is64BitProcess;
             if (!Imports.IsWow64Process(processHandle, out Is64BitProcess))
-                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to determine if process handle {1} is 64 bit",
+                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to determine if process handle 0x{1} is 64 bit",
                     Marshal.GetLastWin32Error(), processHandle.DangerousGetHandle().ToString("X")));
             return !Is64BitProcess;
         }
@@ -38,7 +38,7 @@ namespace BlueMagic.Native
         {
             StringBuilder stringBuilder = new StringBuilder(char.MaxValue);
             if (Imports.GetClassName(windowHandle, stringBuilder, stringBuilder.Capacity) == 0)
-                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to get class name from window {1}",
+                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to get class name from window handle 0x{1}",
                     Marshal.GetLastWin32Error(), windowHandle.ToString("X")));
             return stringBuilder.ToString();
         }
@@ -46,7 +46,7 @@ namespace BlueMagic.Native
         public static bool CloseHandle(IntPtr handle)
         {
             if (!Imports.CloseHandle(handle))
-                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to close handle {0}",
+                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to close handle 0x{1}",
                     Marshal.GetLastWin32Error(), handle.ToString("X")));
             return true;
         }
@@ -69,22 +69,22 @@ namespace BlueMagic.Native
             return bytesWritten;
         }
 
-        public static MemoryBasicInformation Query(SafeMemoryHandle processHandle, IntPtr address, IntPtr sizeBytes)
-        {
-            MemoryBasicInformation memInfo;
-            if (Imports.VirtualQueryEx(processHandle, address, out memInfo, sizeBytes) == 0)
-                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to retrieve memory information from 0x{1}[Size: {2}]",
-                    Marshal.GetLastWin32Error(), address.ToString("X8"), sizeBytes));
-            return memInfo;
-        }
-
         public static MemoryProtectionType ChangeMemoryProtection(SafeMemoryHandle processHandle, IntPtr address, IntPtr sizeBytes, MemoryProtectionType newProtect)
         {
             MemoryProtectionType oldProtect;
             if (!Imports.VirtualProtectEx(processHandle, address, sizeBytes, newProtect, out oldProtect))
-                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to change memory protection at 0x{1}[Size: {2}] to {3}",
-                    Marshal.GetLastWin32Error(), address.ToString("X8"), sizeBytes, newProtect.ToString("X")));
+                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to change memory protection of process handle 0x{1} at 0x{2}[Size: {3}] to {4}",
+                    Marshal.GetLastWin32Error(), processHandle.DangerousGetHandle().ToString("X"), address.ToString("X8"), sizeBytes, newProtect.ToString("X")));
             return oldProtect;
+        }
+
+        public static MemoryBasicInformation Query(SafeMemoryHandle processHandle, IntPtr address, IntPtr sizeBytes)
+        {
+            MemoryBasicInformation memInfo;
+            if (Imports.VirtualQueryEx(processHandle, address, out memInfo, sizeBytes) == 0)
+                throw new Win32Exception(string.Format("[Error Code: {0}] Unable to retrieve memory information of process handle 0x{1} from 0x{2}[Size: {3}]",
+                    Marshal.GetLastWin32Error(), processHandle.DangerousGetHandle().ToString("X"), address.ToString("X8"), sizeBytes));
+            return memInfo;
         }
     }
 }
