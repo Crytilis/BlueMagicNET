f1cc74f73d4620a1770ab42bd89dee964b36017a
Ashley <saeshyls@gmail.com>
2017-05-16 03:34:21 +0100

    Add Test Project

---
+++ BlueMagicTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using BlueMagic;
+using BlueMagic.Native;
+using BlueMagic.Memory;
+
+namespace BlueMagicTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
