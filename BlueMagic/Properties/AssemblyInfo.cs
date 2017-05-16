673914f10c4ffab7f4041d70e08fffd752907167
Ashley <saeshyls@gmail.com>
2017-05-16 03:58:37 +0100

    Update AssemblyInfo

--- BlueMagic/Properties/AssemblyInfo.cs
+++ BlueMagic/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blue Magic")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blue Magic")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("BlueMagic")]
+[assembly: AssemblyDescription("BlueMagic")]
+[assembly: AssemblyProduct("BlueMagic")]
+[assembly: AssemblyCopyright("Copyright © 2017 Ashley Saeshyl")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
